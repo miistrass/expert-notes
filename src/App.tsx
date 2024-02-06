e85f547fefc7481d6f4a7247ec6a0bc4ba601433
Milena Strassburger <milenastrass@gmail.com>
2024-02-06 17:30:15 -0300

    adicionando e configurando o formulário

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -11,15 +12,18 @@ export function App() {
         <input 
           type="text" 
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline:none placeholder:text-slate-500" />
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" />
       </form>
 
       <div className="h-px bg-slate-700"/>
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        
         <NewNoteCard />
-        <NoteCard />
+        
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }}/>
       </div>
 
     </div>
